f73f56e7be1f12dd048a621388900e68fe4c6f07
Kaz Nyborg-Andersen <113869318+kaznyborg@users.noreply.github.com>
2023-04-03 16:33:10 -0700

    feature/ADD

--- db/departments.js
+++ db/departments.js
@@ -1,3 +1,4 @@
+const inquirer = require("inquirer");
 const db = require("./connection");
 
 async function viewAllDepartments() {
@@ -11,4 +12,30 @@ async function viewAllDepartments() {
     }
 }
 
-module.exports = {viewAllDepartments}
+//create addDepartment
+
+async function addDepartment(){
+    try{
+        const departments = await viewAllDepartments();
+        const {
+            name,
+        } = await inquirer.prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: "What department would you like to add?"
+            }
+        ])
+        await db.query(`INSERT into department (name) VALUES ("${name}")`)
+        const newDepartment = await viewAllDepartments();
+        return newDepartment
+    } catch (err){
+        console.log(err)
+    }
+}
+
+//create removeDepartment
+
+
+
+module.exports = {viewAllDepartments, addDepartment}
